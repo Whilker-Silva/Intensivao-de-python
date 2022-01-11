40bd251ff8c13eefa4f258c649a35c4f1481a285
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2022-01-11 20:34:15 -0300

    inicio aula 2

---
+++ Aula 2/aula_2.py
@@ -0,0 +1,11 @@
+# Importar a base de dados
+# Visualizar a base de dados
+# Tratamento de dados (Corrigir os problemas da base de dados)
+# Análise inicial 
+# Análise dethalada dos clientes
+
+import pandas as pd
+
+tabela = pd.read_csv("telecom_users.csv")
+
+print(tabela)
