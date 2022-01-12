3775b7d0542ccd5f18c11cfa3612db9728c0d446
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2022-01-12 00:12:36 -0300

    Configurando VSCode

--- Aula 2/aula_2.py
+++ Aula 2/aula_2.py
@@ -1,11 +1,21 @@
-# Importar a base de dados
-# Visualizar a base de dados
 # Tratamento de dados (Corrigir os problemas da base de dados)
-# Análise inicial 
+# Análise inicial
 # Análise dethalada dos clientes
 
 import pandas as pd
 
-tabela = pd.read_csv("telecom_users.csv")
+# Importar a base de dados
+tabela = pd.read_csv(r"C:\Users\whilk\Desktop\telecom_users.csv")
 
+# Tratamento de dados (Corrigir os problemas da base de dados)
+# Coluna inúltil
+# Valores reconhecidos de forma errada
+# Tratar valores vazios
+
+
+# axis = 0 -> lihhacle
+# axis = 1 -> coluna
+tabela = tabela.drop("Unnamed: 0", axis=1)
 print(tabela)
+
+
